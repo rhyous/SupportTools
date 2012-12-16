b1a0f4aa30c16d8ece59478973f403550f5024db
rhyous <rhyous@yahoo.com>
2012-12-15 18:25:56 -0700

    Add a test project to start writing unit tests

---
+++ SupportToolsTests/SupportToolsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SupportToolsTests
+{
+    [TestClass]
+    public class SupportToolsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
