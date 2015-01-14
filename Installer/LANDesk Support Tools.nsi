1c9eedc872c6f2ac973c7b6c54f07053533febb7
rhyous <rhyous@yahoo.com>
2015-01-14 11:57:33 -0700

    Signed with Rhyous.pxf key. Private key not uploaded.

--- Installer/LANDesk Support Tools.nsi
+++ Installer/LANDesk Support Tools.nsi
@@ -7,9 +7,9 @@ SetCompressor lzma
 
 # General Symbol Definitions
 !define ProjName "Support Tools for LDMS"
-!define ProjVersion "Beta_9.6.0"
+!define ProjVersion "Beta_9.6.1"
 !define REGKEY "SOFTWARE\${ProjName}"
-!define VERSION 9.6.0
+!define VERSION 9.6.1
 !define COMPANY "Rhyous (Jared Barneck)"
 !define URL "http://www.rhyous.com/programming-development/landesk-add-ons/landesk-support-tools/"
 
--- ServiceManager/Properties/AssemblyInfo.cs
+++ ServiceManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,12 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.6.0.0")]
-[assembly: AssemblyFileVersion("9.6.0.0")]
+[assembly: AssemblyVersion("9.6.1.2")]
+[assembly: AssemblyFileVersion("9.6.1.2")]
 
-[assembly: InternalsVisibleTo("AspectMVVM")]
+[assembly: InternalsVisibleTo("AspectMVVM,PublicKey=" +
+                              "002400000480000094000000060200000024000052534131000400000100010009321c9dc95531" +
+                              "36f36198f762aa60f3a4c9d0735458dbec4eeceea1d66e00478fdb08fd7736add9083db0f873ec" +
+                              "f778d64b9810acf7f46b7f1dbf44b0a3b0c2a815972eaa0f57eb27a62d6d8b8ec8f04cd86215c4" +
+                              "5c4cbb3aac53345106aa8fc212e39ba73b7c02dafe177c97e6c9845e62ec1eb768d50b5eb25a0d" +
+                              "f869c696")]
